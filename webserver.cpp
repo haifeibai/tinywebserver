5c6e47640ed0247cc1a00106aa2e0c58753b3dd8
haifeibai <2894720700@qq.com>
2024-05-13 14:18:05 +0800

    debug

--- webserver.cpp
+++ webserver.cpp
@@ -87,7 +87,7 @@ void WebServer::log_write()
 void WebServer::sql_pool()
 {
     //初始化数据库连接池
-    m_connPool = connection_pool::GetInstance();
+    m_connPool = connection_pool::GetInstance();//创建connection_pool的对象
     m_connPool->init("localhost", m_user, m_passWord, m_databaseName, 3306, m_sql_num, m_close_log);
 
     //初始化数据库读取表
